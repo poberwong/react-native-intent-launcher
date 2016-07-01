c928bb625b17ef7793fe14a05f8a3c931a63cfa5
poberWong <poberwong@gmail.com>
2016-07-01 21:12:18 +0800

    construct lib

---
+++ lib/android/src/main/java/com/poberwong/IntentLauncherModule.java
@@ -0,0 +1,61 @@
+package com.poberwong;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
+
+import java.io.Console;
+
+/**
+ * Created by poberwong on 16/6/30.
+ */
+public class IntentLauncherModule extends ReactContextBaseJavaModule {
+    private static final String ATTR_ACTION = "action";
+    private static final String ATTR_CATEGORY = "category";
+    private static final String TAG_EXTRA = "extra";
+    private static final String ATTR_DATA = "data";
+    private static final String ATTR_FLAGS = "flags";
+
+    public IntentLauncherModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    public String getName() {
+        return "IntentLauncher";
+    }
+
+    /**
+     * 选用方案
+     * intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+     * getReactApplicationContext().startActivity(intent);
+     */
+    @ReactMethod
+    public void startActivity(ReadableMap params){
+        Intent intent = new Intent();
+        if (params.hasKey(ATTR_DATA)) {
+            intent.setData(Uri.parse(params.getString(ATTR_DATA)));
+        }
+        if (params.hasKey(TAG_EXTRA)) {
+            intent.putExtras(Arguments.toBundle(params.getMap(TAG_EXTRA)));
+        }
+        if (params.hasKey(ATTR_FLAGS)) {
+            intent.addFlags(params.getInt(ATTR_FLAGS));
+        }
+        if (params.hasKey(ATTR_CATEGORY)) {
+            intent.addCategory(params.getString(ATTR_CATEGORY));
+        }
+        if (params.hasKey(ATTR_ACTION)) {
+            intent.setAction(params.getString(ATTR_ACTION));
+        }
+        getReactApplicationContext().startActivityForResult(intent, 0, null); // 暂时使用当前应用的任务栈
+    }
+}
---
+++ lib/android/src/main/java/com/poberwong/IntentLauncherPackage.java
@@ -0,0 +1,31 @@
+package com.poberwong;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by poberwong on 16/6/30.
+ */
+public class IntentLauncherPackage implements ReactPackage{
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        return Arrays.<NativeModule>asList(new IntentLauncherModule(reactContext)); // 返回一个NativeModule范型的数组就ok
+    }
+
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
